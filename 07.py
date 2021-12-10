a97e394e20542cf7069c7b62e4ef201b41e517c3
Jason Hussey <jphussey05@gmail.com>
2021-12-09 20:00:43 -0700

    day7 complete

--- 07.py
+++ 07.py
@@ -1,19 +1,23 @@
 from utils import get_input_file
 import numpy as np
-
+from pprint import pprint
 
 if __name__ == '__main__':
 
-    positions = [int(num) for num in get_input_file('07-test.txt')[0].split(',')]
+    positions = [int(num) for num in get_input_file('07.txt')[0].split(',')]
 
-    fuel = 0
-    tgt = np.mean(positions)
+    
+    lower = min(positions)
+    upper = max(positions)
+    fuel_costs = {}
 
-    for pos in positions:
-        steps = abs(pos-tgt)
-        fuel += (steps * (steps + 1)) / 2
+    for tgt in range(lower, upper+1):
+        fuel = 0
+        for pos in positions:
+            steps = abs(pos-tgt)
+            fuel += (steps * (steps + 1)) / 2
+        
+        fuel_costs[tgt] = fuel
 
-
-
-    print(np.ceil(fuel))
+    pprint(fuel_costs[sorted(fuel_costs, key=lambda x: fuel_costs[x])[0]])
     

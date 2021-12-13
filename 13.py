246c0eb4f8b608e2306e6d28e47719f7e65faceb
Jason <jphussey05@gmail.com>
2021-12-13 00:37:57 -0700

    day 13 ph

---
+++ 13.py
@@ -0,0 +1,22 @@
+from utils import get_input_file
+
+
+
+
+
+
+if __name__ == '__main__':
+    contents = get_input_file('13.txt')
+
+    break_spot = contents.index('')
+
+    folds = contents[break_spot+1:]
+    marks = contents[:break_spot]
+
+    rows = [int(x.split(',')[0]) for x in marks]
+    cols = [int(x.split(',')[1]) for x in marks]
+    print(min(rows), max(rows))
+    print(min(cols), max(cols))
+
+    for fold in folds:
+        print(fold)

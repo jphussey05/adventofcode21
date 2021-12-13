6cc8ab6fad1279b1691b64edd585f00596ba066c
Jason <jphussey05@gmail.com>
2021-12-13 09:46:00 -0700

    day 13 in progress

--- 13.py
+++ 13.py
@@ -6,7 +6,7 @@ from utils import get_input_file
 
 
 if __name__ == '__main__':
-    contents = get_input_file('13.txt')
+    contents = get_input_file('13-test.txt')
 
     break_spot = contents.index('')
 
@@ -15,8 +15,11 @@ if __name__ == '__main__':
 
     rows = [int(x.split(',')[0]) for x in marks]
     cols = [int(x.split(',')[1]) for x in marks]
+    row_num = max(rows)
     print(min(rows), max(rows))
     print(min(cols), max(cols))
 
     for fold in folds:
         print(fold)
+
+    for mark in marks:
